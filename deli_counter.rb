0264058a3bd44fb27eb2e724d6dcf8968bc06785
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-09 16:59:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(katz_deli_line)
   if katz_deli_line.length == 0
     puts "The line is currently empty."
   else
-   line = "The line is currently:"
+   line = "The line is currently: "
      katz_deli_line.each.with_index(1) do |person, i|
       line << " #{i}. #{person}"
       end
